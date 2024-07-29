8adadf98f229828818d2a2e3c012862f81a2d083
jjaney <hmyeonge@gmail.com>
2024-07-29 10:15:15 +0900

    [Bronze V] Title: 나머지, Time: 124 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.js
@@ -0,0 +1,10 @@
+let input = require("fs").readFileSync(0).toString().trim().split(" ");
+
+const A = Number(input[0]);
+const B = Number(input[1]);
+const C = Number(input[2]);
+
+console.log((A + B) % C);
+console.log(((A % C) + (B % C)) % C);
+console.log((A * B) % C);
+console.log(((A % C) * (B % C)) % C);
