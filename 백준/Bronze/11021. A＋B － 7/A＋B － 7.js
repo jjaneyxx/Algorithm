821c23efec081d8da4755bb2951c97f7bed4541c
jjaney <hmyeonge@gmail.com>
2024-08-04 13:26:24 +0900

    [Bronze V] Title: A+B - 7, Time: 100 ms, Memory: 9516 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -1,16 +1,8 @@
 let input = require("fs").readFileSync(0).toString().trim().split("\n");
 
-// console.log(input);
 let T = +input[0];
 
-/* 
-_[1],..._[4] : 문자열 -> 배열, 숫자 
-배열[0]+배열[1] -> for 문
-*/
-
 for (let i = 1; i <= T; i++) {
-  let sum = 0;
   input[i] = input[i].split(" ").map((v) => +v);
-  sum = input[i][0] + input[i][1];
-  console.log(`Case #${i}: ${sum}`);
+  console.log(`Case #${i}: ${input[i][0] + input[i][1]}`);
 }
