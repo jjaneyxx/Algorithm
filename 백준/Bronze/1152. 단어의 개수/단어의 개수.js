6eef810209cdc3168b746d86237c02a52c44da7a
jjaney <hmyeonge@gmail.com>
2024-08-13 16:19:29 +0900

    [Bronze II] Title: 단어의 개수, Time: 148 ms, Memory: 18340 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.js
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.js
@@ -1,11 +1,2 @@
-let input = require("fs").readFileSync(0).toString().trim().split(" ");
-let count = 0;
-
-for (let i = 0; i < input.length; i++) {
-  // 입력이 공백 하나일 때를 대비, 요소가 공백이 아닐 때만 카운트
-  if (input[i] !== "") {
-    count++;
-  }
-}
-
-console.log(count);
+let num = `${require("fs").readFileSync(0)}`.trim();
+console.log(num ? num.split(" ").length : 0);
