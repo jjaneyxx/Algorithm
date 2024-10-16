ed58143c3f79b9e4fecca1baec6cdd26b9afa766
jjaney <hmyeonge@gmail.com>
2024-10-16 17:29:27 +0900

    [Bronze II] Title: 분해합, Time: 180 ms, Memory: 9764 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.js
+++ 백준/Bronze/2231. 분해합/분해합.js
@@ -1,23 +1,22 @@
-const input = Number(require("fs").readFileSync(0).toString().trim());
+// 분해합 N의 가장 작은 생성자를 출력하기. 없으면 0 출력하기
+// O(1), O(N), O(logN) 가능
 
-let result = 0; // 생성자, 못 찾으면 0 
+const N = Number(require("fs").readFileSync(0).toString().trim()); // 216 -> 198
+let answer = 0;
+// 생성자는 1~N-1
+for (let i = 0; i < N; i++) {
+  let sum = i; // 생성자의 합은 생성자에서 먼저 시작
+  let num = i;
 
-// 생성자는 1부터 입력값 -1 사이에 존재하므로 이 구간을 탐색함 
-for (let i = 0; i < input; i++) {
-  let sum = i; // 현재 숫자 자체를 먼저 저장, 이후 각 자릿수도 더해줄 예정
-  let num = i; // 자릿수를 계산하기 위한 임의의 변수 
-  // num(i)의 각 자릿수를 모두 더하는 반복문 
   while (num > 0) {
-    sum += num % 10; // 가장 오른쪽 자릿수를 sum 에 더함 
-    num = Math.floor(num / 10); // num의 가장 오른쪽 자릿수를 제거
+    sum += num % 10;
+    num = Math.floor(num / 10);
   }
-
-  // sum : i + i 를 쪼갠 자릿수의 합, input : 입력된 분해합
-  // 둘이 같다 = 현재 i 가 분해합의 가장 작은 생성자 
-  if (sum === input) {
-    result = i; 
-    break; // 답을 찾았으므로 전체 for문 종료 
+  // sum = sum + 각 자릿수 합
+  // sum 이 N과 같다면, 생성자를 찾은 것
+  if (sum === N) {
+    answer = i;
+    break;
   }
 }
-
-console.log(result);
+console.log(answer);
